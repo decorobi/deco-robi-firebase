d9d3237e51552875ca68304c772bf869e6a9cc0f
decorobi <info@decorobi.it>
2026-02-01 08:49:40 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import PinGate from './PinGate'
 
-createRoot(document.getElementById('root')!).render(<App />)
+createRoot(document.getElementById('root')!).render(
+  <PinGate>
+    <App />
+  </PinGate>
+)
